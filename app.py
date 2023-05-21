7b6fa82a746ce95319ebcc69bb9c371f4ec7ce07
Kavish Devar <developer@sendildevar.in>
2023-05-21 17:09:40 +0530

    Cleanup HW View

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def homeworkAPI(id):
     return jsonify(user.hwlist[str(id)])
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0',port=9090, debug=True)
+    app.run(host='0.0.0.0',port=8080, debug=True)
