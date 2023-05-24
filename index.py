dcec860beed504aaa3829d1949537c78fd871539
Kavish Devar <developer@sendildevar.in>
2023-05-24 20:27:06 +0530

    update topappbar and rollback to azure

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import requests
 
 app=Flask(__name__)
 
-baseurl = "api.kedunext.stiblook.rocks"
+baseurl = "kedunext.azurewebsites.net"
 protocol = "https"
 
 import time
@@ -24,13 +24,10 @@ class User:
         self.hwlist = requests.post(f"{protocol}://{baseurl}/api/v1/homeworks", data={"username": self.username, "password": self.password}).json()
         self.circularlist = requests.post(f"{protocol}://{baseurl}/api/v1/circulars", data={"username": self.username, "password": self.password}).json()
         self.photoURL = requests.post(f"{protocol}://{baseurl}/api/v1/photo", data={"username": self.username, "password": self.password}).text
-        self.retrieved = time.time()
 
 @app.route('/login',methods=["GET","POST"])
 def login():
     if request.method=="POST":
-        # print(request.form)
-        # user=pydunext.User(request.form['username'],request.form['password'])
         resp=make_response(redirect(url_for('index')))
         resp.set_cookie("username",request.form['username'])
         resp.set_cookie("password",request.form['password'])
@@ -42,33 +39,46 @@ def login():
 def homework():
     if request.cookies.get('username')==None:
         return redirect(url_for('login'))
-    # if 'user'+request.cookies['username'] not in globals():# or time.time() - globals()['user'+request.cookies['username']].retrieved > 10:
-        # globals()['user'+request.cookies['username']]=(request.cookies['username'],request.cookies['password'])
     user=User(request.cookies['username'],request.cookies['password'],"hw")
-    print(sorted(user.hwlist.keys(),reverse=True))
-    return render_template('homeworks.html',homeworks=user.hwlist,ids=sorted(user.hwlist.keys(),reverse=True),photoURL=user.photoURL)
-    # return render_template('homeworks.html',homeworks=globals()['user'+request.cookies['username']].hwlist,ids=globals()['user'+request.cookies['username']].hwlist.keys(),photoURL=globals()['user'+request.cookies['username']].photoURL)
-
+    if "error" in user.hwlist.keys():
+        return render_template('homeworks.html',error=user.hwlist["error"],homeworks={},ids=[],photoURL="")
+    else:
+        return render_template('homeworks.html',homeworks=user.hwlist,ids=sorted(user.hwlist.keys(),reverse=True),photoURL=user.photoURL)
+from datetime import datetime
 @app.route('/circulars')
 def circular():
     if request.cookies.get('username')==None:
         return redirect(url_for('login'))
-    # if 'user'+request.cookies['username'] not in globals():# or time.time() - globals()['user'+request.cookies['username']].retrieved > 10:
-    #     globals()['user'+request.cookies['username']]=pydunext.User(request.cookies['username'],request.cookies['password'])
     user=User(request.cookies['username'],request.cookies['password'],"circular")
-    return render_template('circulars.html',circulars=user.circularlist,ids=reversed(sorted(user.circularlist.keys())),photoURL=user.photoURL)
-    # return render_template('circulars.html',circulars=globals()['user'+request.cookies['username']].circularlist,ids=globals()['user'+request.cookies['username']].circularlist.keys(),photoURL=globals()['user'+request.cookies['username']].photoURL)
-
+    if "error" in user.circularlist.keys():
+        return render_template('circulars.html',error=user.circularlist["error"],homeworks={},ids=[],photoURL="")
+    else:
+        keys=user.circularlist.keys()
+        dates=[]
+        for key in keys:
+            # user.circularlist[key]["date"]=datetime.strptime(user.circularlist[key]["date"], '%d-%m-%Y').date()
+            print(user.circularlist[key]["date"])
+            dates.append(user.circularlist[key]["date"])
+        dates.sort(key = lambda date: datetime.strptime(date, '%d-%m-%Y'), reverse=True)
+        ids=[]
+        for date in dates:
+            for key in keys:
+                if user.circularlist[key]["date"]==date:
+                    ids.append(key)
+                    break
+            print(date)
+        print(ids)
+        return render_template('circulars.html',circulars=user.circularlist,ids=ids,photoURL=user.photoURL)
 
 @app.route('/')
 def index():
     if request.cookies.get('username')==None:
         return redirect(url_for('login'))
-    # if 'user'+request.cookies['username'] not in globals():# or time.time() - globals()['user'+request.cookies['username']].retrieved > 10:
-    #     globals()['user'+request.cookies['username']]=pydunext.User(request.cookies['username'],request.cookies['password'])
     user=User(request.cookies['username'],request.cookies['password'])
-    return render_template('index.html',homeworks=user.hwlist,ids=reversed(sorted(user.hwlist.keys())),photoURL=user.photoURL,circulars=user.circularlist,circularids=reversed(sorted(user.circularlist.keys())))
-    # return render_template('index.html',homeworks=globals()['user'+request.cookies['username']].hwlist,ids=globals()['user'+request.cookies['username']].hwlist.keys(),photoURL=globals()['user'+request.cookies['username']].photoURL,circulars=globals()['user'+request.cookies['username']].circularlist,circularids=globals()['user'+request.cookies['username']].circularlist.keys())
-
+    if "error" in user.hwlist.keys() or "error" in user.circularlist.keys():
+        return render_template('index.html',error=user.hwlist["error"],homeworks={},ids=[],photoURL="")
+    else:
+        return render_template('index.html',homeworks=user.hwlist,ids=reversed(sorted(user.hwlist.keys())),photoURL=user.photoURL,circulars=user.circularlist,circularids=reversed(sorted(user.circularlist.keys())))
+    
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=8000, debug=True)
